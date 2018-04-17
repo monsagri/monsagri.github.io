7be361662a2a3278c5dc7b7bbedf97afa0437bd6
Fabian Feldberg <feldbergf@gmail.com>
2018-04-17 14:17:49 +0100

    added font and TypeIt

--- js/app.js
+++ js/app.js
@@ -1,27 +1,11 @@
+/* global TypeIt */
+
 console.log('JS locked and loaded');
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
+  new TypeIt('.hero-header__subtitle', {
+    strings: ['This site is currently under construction.', 'Come back soon!']
+  });
 
 });
