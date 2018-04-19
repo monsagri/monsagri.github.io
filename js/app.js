de8c759852350c6007c7edd482da83f34c749505
Fabian Feldberg <feldbergf@gmail.com>
2018-04-19 11:41:36 +0100

    started working on Project Display, need to adjust image sizing. added onClick to scroll down to about. Added jQuery

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 /* global TypeIt */
-
 console.log('JS locked and loaded');
 
-document.addEventListener('DOMContentLoaded', function () {
+//Check for DOM loaded
+$(function() {  // run DOM related functions
+  init();
+});
+
+// Stuff to do once DOM is loaded
+function init(){
+
+  console.log('DOM Loaded');
 
   new TypeIt('.hero-header__subtitle', {
     strings: ['This site is currently under construction.', 'Come back soon!']
   });
 
-});
+  // DOM variables
+  const $about = $('.about');
+  const $chevronDiv = $('#chevronDiv');
+  const $typeItContainer = $('.ti-container')[0];
+
+  function scrollToAbout()  {
+    console.log('moving down');
+    $('html, body').animate({
+      scrollTop: $about.offset().top
+    }, 2000);
+  }
+
+  $chevronDiv.on('click',scrollToAbout );
+
+}
